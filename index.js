26143d269eee2dc32e2e2c4e6ceb3c36f357783b
Joseph <zhao.joseph@yahoo.ca>
2023-06-11 00:25:24 -0400

    Feat: removed cron jobs, to seperate function

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import app from './app.js'
 import  http from 'http'
 import EventEmitter from 'events'
 import process from 'node:process';
-import { fork } from 'child_process';
-import { CronJob } from 'cron';
 
 const server = http.createServer(app)
 
@@ -11,28 +9,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const { PORT, FILENAME } = process.env;
-
-var jobs = [
-  new CronJob(
-  "00 */10 * * * *",
-  function() {
-    console.log("Scraper started.")
-    const process = fork('./scrape.js');
-  },
-  null,
-  false,
-  'America/Toronto'),
-  new CronJob(
-  "00 5 * * * *",
-  function() {
-    console.log("Removing Expired.")
-    const process = fork('./expired.js');
-  }),
-]
-
-jobs.forEach(function(job) {
-  job.start()
-})
 
 process.on('warning', (warning) => {
 
